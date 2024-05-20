7063cb347dd3c3772870cf35e8084d6c34edd407
gabrielabade <gabriela.bade@hotmail.com>
2024-05-20 16:17:59 -0300

    mascara-formulario

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
-function cliqueiNoBotao(){
-  alert("")
+const form = document.querySelector(".formulario-fale-conosco");
+const background = document.querySelector(".mascara-formulario");
+
+function showForm() {
+  form.style.left = "50%";
+  form.style.transform = "translateX(-50%)";
+  background.style.visibility = "visible";
+}
+
+function hideForm() {
+  form.style.left = "-330px";
+  form.style.transform = "translateX(0)";
+  background.style.visibility = "hidden";
 }
